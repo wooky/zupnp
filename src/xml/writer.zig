413a0ac57cece2321c5112363652c1873127e004
wjykk <no@no>
2021-02-28 20:06:22 -0700

    Adapt XML writer for struct traverser

--- src/xml/writer.zig
+++ src/xml/writer.zig
@@ -5,7 +5,7 @@ const Error = error.XMLWriteError;
 
 const XMLWriter = @This();
 const logger = std.log.scoped(.XMLWriter);
-const attributes_field_name = "__attributes__";
+usingnamespace @import("traverser.zig").XMLStructTraverser(XMLWriter);
 
 arena: ArenaAllocator,
 doc_handle: [*c]c.IXML_Document = undefined,
@@ -40,40 +40,66 @@ pub fn writeStructToDocumentHandle(self: *XMLWriter, input: anytype) !*c.IXML_Do
         return Error;
     }
 
-    try self.readFromStruct(input, @ptrCast(*c.IXML_Node, self.doc_handle));
+    try self.traverseStruct(&input, @ptrCast(*c.IXML_Node, self.doc_handle));
     return self.doc_handle;
 }
 
-fn readFromStruct(self: *XMLWriter, input: anytype, parent: *c.IXML_Node) !void {
-    inline for (@typeInfo(@TypeOf(input)).Struct.fields) |field| {
-        try self.readField(@field(input, field.name), field.name, parent);
+pub fn precheckNode(self: *XMLWriter, node: *c.IXML_Node) !void {
+    // No need to check
+}
+
+pub fn handleSubStruct(self: *XMLWriter, comptime name: []const u8, input: anytype, parent: *c.IXML_Node) !void {
+    var sub_parent = try self.createNode(name);
+    try self.traverseStruct(input, sub_parent);
+    try append(parent, sub_parent);
+}
+
+pub fn handlePointer(self: *XMLWriter, comptime name: []const u8, input: anytype, parent: *c.IXML_Node) !void {
+    for (input.*) |subinput| {
+        try self.traverseField(&subinput, name, parent);
     }
 }
 
-fn readField(self: *XMLWriter, input: anytype, comptime name: []const u8, parent: *c.IXML_Node) !void {
-    switch (@typeInfo(@TypeOf(input))) {
-        .Struct => |s| {
-            if (comptime std.mem.eql(u8, name, attributes_field_name)) {
-                try self.writeAttributes(input, @ptrCast(*c.IXML_Element, parent));
+pub fn handleOptional(self: *XMLWriter, comptime name: []const u8, input: anytype, parent: *c.IXML_Node) !void {
+    if (input.*) |i| {
+        try self.traverseField(&i, name, parent);
+    }
+}
+
+pub fn handleString(self: *XMLWriter, comptime name: []const u8, input: anytype, parent: *c.IXML_Node) !void {
+    var node = try self.createNode(name);
+    var text: [*c]c.IXML_Node = undefined;
+
+    {
+        const err = c.ixmlDocument_createTextNodeEx(self.doc_handle, name ++ "\x00", &text);
+        if (err != c.IXML_SUCCESS) {
+            logger.err("Failed to create text node: {}", .{@intToEnum(c.IXML_ERRORCODE, err)});
+            return Error;
+        }
+    }
+
+    {
+        const err = c.ixmlNode_setNodeValue(text, try self.arena.allocator.dupeZ(u8, input.*));
+        if (err != c.IXML_SUCCESS) {
+            logger.err("Failed to set text node value: {}", .{@intToEnum(c.IXML_ERRORCODE, err)});
+            return Error;
+        }
+    }
+
+    try append(node, text);
+    try append(parent, node);
+}
+
+pub fn handleAttributes(self: *XMLWriter, comptime name: []const u8, input: anytype, node: *c.IXML_Node) !void {
+    inline for (@typeInfo(@TypeOf(input.*)).Struct.fields) |field| {
+        const field_value_opt: ?[]const u8 = @field(input.*, field.name);
+        if (field_value_opt) |field_value| {
+            const err = c.ixmlElement_setAttribute(@ptrCast(*c.IXML_Element, node), field.name ++ "\x00", try self.arena.allocator.dupeZ(u8, field_value));
+            if (err != c.IXML_SUCCESS) {
+                logger.err("Failed to set attribute: {}", .{@intToEnum(c.IXML_ERRORCODE, err)});
+                return Error;
             }
-            else {
-                var sub_parent = try self.createNode(name);
-                try self.readFromStruct(input, sub_parent);
-                try append(parent, sub_parent);
-            }
-        },
-        .Pointer => |p| {
-            if (p.child == u8) {
-                try self.writeString(name, input, parent);
-            }
-            else {
-                for (input) |subinput| {
-                    try self.readField(subinput, name, parent);
-                }
-            }
-        },
-        .Optional => if (input) |i| try self.readField(i, name, parent),
-        else => @compileError("Invalid field '" ++ input_info.name ++ "' inside struct")
+        }
     }
 }
 
@@ -92,42 +118,5 @@ fn append(parent: *c.IXML_Node, child: *c.IXML_Node) !void {
     if (err != c.IXML_SUCCESS) {
         logger.err("Failed to append node: {}", .{@intToEnum(c.IXML_ERRORCODE, err)});
         return Error;
-    }
-}
-
-fn writeString(self: *XMLWriter, comptime name: []const u8, value: []const u8, parent: *c.IXML_Node) !void {
-    var node = try self.createNode(name);
-    var text: [*c]c.IXML_Node = undefined;
-
-    {
-        const err = c.ixmlDocument_createTextNodeEx(self.doc_handle, name ++ "\x00", &text);
-        if (err != c.IXML_SUCCESS) {
-            logger.err("Failed to create text node: {}", .{@intToEnum(c.IXML_ERRORCODE, err)});
-            return Error;
-        }
-    }
-
-    {
-        const err = c.ixmlNode_setNodeValue(text, try self.arena.allocator.dupeZ(u8, value));
-        if (err != c.IXML_SUCCESS) {
-            logger.err("Failed to set text node value: {}", .{@intToEnum(c.IXML_ERRORCODE, err)});
-            return Error;
-        }
-    }
-
-    try append(node, text);
-    try append(parent, node);
-}
-
-fn writeAttributes(self: *XMLWriter, input: anytype, element: *c.IXML_Element) !void {
-    inline for (@typeInfo(@TypeOf(input)).Struct.fields) |field| {
-        const field_value_opt: ?[]const u8 = @field(input, field.name);
-        if (field_value_opt) |field_value| {
-            const err = c.ixmlElement_setAttribute(element, field.name ++ "\x00", try self.arena.allocator.dupeZ(u8, field_value));
-            if (err != c.IXML_SUCCESS) {
-                logger.err("Failed to set attribute: {}", .{@intToEnum(c.IXML_ERRORCODE, err)});
-                return Error;
-            }
-        }
     }
 }
